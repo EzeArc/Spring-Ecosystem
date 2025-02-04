f5e0e470c87edcb334ef80348be97afe4183f287
Ezequiel <ezequielarce_1996@hotmail.com>
2025-02-04 17:40:36 -0300

    feat(rest-api): create base proyect

---
+++ rest-api-template/src/main/java/spring/ecosystem/rest_api_template/RestApiTemplateApplication.java
@@ -0,0 +1,13 @@
+package spring.ecosystem.rest_api_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiTemplateApplication.class, args);
+	}
+
+}
---
+++ rest-api-template/src/test/java/spring/ecosystem/rest_api_template/RestApiTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.ecosystem.rest_api_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
