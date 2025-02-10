a14b477cf14d310db3df788205f9190e3dbe2165
Ezequiel <ezequielarce_1996@hotmail.com>
2025-02-10 18:06:22 -0300

    feat(spelling): fix misspelled

--- rest-api-template/src/main/java/spring/ecosystem/rest_api_template/configSecurity/filter/JwtAutheticateFilter.java
+++ rest-api-template/src/main/java/spring/ecosystem/rest_api_template/configSecurity/filter/JwtAutheticateFilter.java
@@ -11,6 +11,8 @@ import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import spring.ecosystem.rest_api_template.entities.User;
 import spring.ecosystem.rest_api_template.services.UserService;
 import spring.ecosystem.rest_api_template.services.auth.JwtService;
 
@@ -22,41 +24,25 @@ public class JwtAutheticateFilter extends OncePerRequestFilter {
     private JwtService jwtService;
 
     @Autowired
-    private UserService usuarioServicio;
+    private UserService userService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        /*agarro en header y busco las autorizaciones*/
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
         String authorizationHeaders = request.getHeader("Authorization");
+
         if (!StringUtils.hasText(authorizationHeaders) || !authorizationHeaders.startsWith("Bearer")) {
             filterChain.doFilter(request, response);
             return;
-
         }
 
-        /* agarro el jwt */
-
         String jwt = authorizationHeaders.split(" ")[1];
-
-        /* extraer el subjet/username  usando el jwtService*/
-        String userName = jwtService.extracUsername(jwt);
-
-        /* setear objete autentificado dentro de ContextHolder*/
-        Usuario user = usuarioServicio.findOneByUsername(userName);
-        /* creo el token de authetificacion para el securyt context holder*/
-
-        UsernamePasswordAuthenticationToken autheToken = new UsernamePasswordAuthenticationToken(userName, null, user.getAuthorities());
-
-        /*agrego detalles para sumar info a la authentificaicon*/
-
-        autheToken.setDetails(new WebAuthenticationDetails(request));
-
-        /*seteo el secunrity context holder*/
-
-        SecurityContextHolder.getContext().setAuthentication(autheToken);
-
+        String userName = jwtService.extractUsername(jwt);
+        User user = userService.findOneByUsername(userName);
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userName, null,
+                user.getAuthorities());
+        authToken.setDetails(new WebAuthenticationDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authToken);
         filterChain.doFilter(request, response);
-
-
     }
 }
--- rest-api-template/src/main/java/spring/ecosystem/rest_api_template/configSecurity/httpSecurityConfig.java
+++ rest-api-template/src/main/java/spring/ecosystem/rest_api_template/configSecurity/httpSecurityConfig.java
@@ -22,92 +22,104 @@ import java.util.Arrays;
 @Configuration
 @EnableWebSecurity
 public class httpSecurityConfig {
-    @Autowired
-    private AuthenticationProvider daoAuthenticationProvider;
+        @Autowired
+        private AuthenticationProvider daoAuthenticationProvider;
 
-    @Autowired
-    private JwtAutheticateFilter jwtAutheticateFilter;
+        @Autowired
+        private JwtAutheticateFilter jwtAuthenticateFilter;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // Configuración de OAuth2
-        // Configuración de JWT
-        http
-                .cors(Customizer.withDefaults())
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(sessionMagConfig -> sessionMagConfig
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(daoAuthenticationProvider)
-                .addFilterBefore(jwtAutheticateFilter, UsernamePasswordAuthenticationFilter.class)
-                .authorizeHttpRequests(authRequestConfig -> {
-                    // Rutas privadas ADMIN
-                    authRequestConfig.requestMatchers(HttpMethod.GET, "/adminController/listaServiciosAdmin")
-                            .hasRole(Role.ADMIN.name());
-                    authRequestConfig.requestMatchers(HttpMethod.GET, "/adminController/listaTurnoAdmin")
-                            .hasRole(Role.ADMIN.name());
-                    authRequestConfig.requestMatchers(HttpMethod.GET, "/api/v1/user").hasRole(Rol.ADMIN.name());
-                    authRequestConfig.requestMatchers(HttpMethod.POST, "/adminController/crearServicio")
-                            .hasRole(Role.ADMIN.name());
-                    authRequestConfig.requestMatchers(HttpMethod.PUT, "/adminController/AltaBajaServicio/{id}")
-                            .hasRole(Role.ADMIN.name());
-                    authRequestConfig.requestMatchers(HttpMethod.PUT, "/adminController/AltaBaja/{id}")
-                            .hasRole(Role.ADMIN.name());
-                    authRequestConfig.requestMatchers(HttpMethod.PUT, "/adminController/ModificarServicio")
-                            .hasRole(Role.ADMIN.name());
-                    // aca es el endpoint para cuando el admin quiere cancerlar el turno especifico de un dia
-                    authRequestConfig.requestMatchers(HttpMethod.PUT, "/adminController/suspendeTurnoAdmin/{turnoId}")
-                            .permitAll();
-                    // endpot para que el admin agregue el feriado a dia en especifico
-                    authRequestConfig.requestMatchers(HttpMethod.POST, "/adminController/agregarFeriadoAdmin")
-                            .permitAll();
-                    authRequestConfig.requestMatchers(HttpMethod.GET, "/adminController/listaTurnoDelMesAdmin/{date}")
-                            .permitAll();
-                    authRequestConfig.requestMatchers(HttpMethod.GET, "/adminController/filtrarTurnoPor")
-                            .permitAll();
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+                http
+                                .cors(Customizer.withDefaults())
+                                .csrf(csrf -> csrf.disable())
+                                .sessionManagement(sessionMagConfig -> sessionMagConfig
+                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .authenticationProvider(daoAuthenticationProvider)
+                                .addFilterBefore(jwtAuthenticateFilter, UsernamePasswordAuthenticationFilter.class)
+                                .authorizeHttpRequests(authRequestConfig -> {
+                                        authRequestConfig
+                                                        .requestMatchers(HttpMethod.GET,
+                                                                        "/adminController/listaServiciosAdmin")
+                                                        .hasRole(Role.ADMIN.name());
+                                        authRequestConfig
+                                                        .requestMatchers(HttpMethod.GET,
+                                                                        "/adminController/listaTurnoAdmin")
+                                                        .hasRole(Role.ADMIN.name());
+                                        authRequestConfig.requestMatchers(HttpMethod.GET, "/api/v1/user")
+                                                        .hasRole(Role.ADMIN.name());
+                                        authRequestConfig
+                                                        .requestMatchers(HttpMethod.POST,
+                                                                        "/adminController/crearServicio")
+                                                        .hasRole(Role.ADMIN.name());
+                                        authRequestConfig
+                                                        .requestMatchers(HttpMethod.PUT,
+                                                                        "/adminController/AltaBajaServicio/{id}")
+                                                        .hasRole(Role.ADMIN.name());
+                                        authRequestConfig
+                                                        .requestMatchers(HttpMethod.PUT,
+                                                                        "/adminController/AltaBaja/{id}")
+                                                        .hasRole(Role.ADMIN.name());
+                                        authRequestConfig
+                                                        .requestMatchers(HttpMethod.PUT,
+                                                                        "/adminController/ModificarServicio")
+                                                        .hasRole(Role.ADMIN.name());
+                                        authRequestConfig
+                                                        .requestMatchers(HttpMethod.PUT,
+                                                                        "/adminController/suspendeTurnoAdmin/{turnoId}")
+                                                        .permitAll();
+                                        authRequestConfig
+                                                        .requestMatchers(HttpMethod.POST,
+                                                                        "/adminController/agregarFeriadoAdmin")
+                                                        .permitAll();
+                                        authRequestConfig
+                                                        .requestMatchers(HttpMethod.GET,
+                                                                        "/adminController/listaTurnoDelMesAdmin/{date}")
+                                                        .permitAll();
+                                        authRequestConfig
+                                                        .requestMatchers(HttpMethod.GET,
+                                                                        "/adminController/filtrarTurnoPor")
+                                                        .permitAll();
 
+                                        authRequestConfig.requestMatchers(HttpMethod.GET, "/api/v1/auth/is-token-valid")
+                                                        .permitAll();
+                                        authRequestConfig.requestMatchers(HttpMethod.GET, "/api/v1/auth/validate")
+                                                        .permitAll();
+                                        authRequestConfig.requestMatchers(HttpMethod.GET, "/api/v1/servicios")
+                                                        .permitAll();
+                                        authRequestConfig.requestMatchers(HttpMethod.GET, "/portal/listaSerivicios")
+                                                        .permitAll();
+                                        authRequestConfig.requestMatchers(HttpMethod.GET, "/portal/servicioPodo/{id}")
+                                                        .permitAll();
+                                        authRequestConfig.requestMatchers(HttpMethod.GET, "/Turnos/cancelarTurno/{id}")
+                                                        .hasRole(Role.USER.name());
+                                        authRequestConfig.requestMatchers(HttpMethod.GET, "/Turnos/listaTurnos/{id}")
+                                                        .hasRole(Role.USER.name());
+                                        authRequestConfig.requestMatchers(HttpMethod.POST, "/api/v1/auth/authenticate")
+                                                        .permitAll();
+                                        authRequestConfig.requestMatchers(HttpMethod.POST, "/api/v1/auth/google")
+                                                        .permitAll();
+                                        authRequestConfig.requestMatchers(HttpMethod.POST, "/api/v1/auth/send-email")
+                                                        .permitAll();
+                                        authRequestConfig.requestMatchers(HttpMethod.POST, "/api/v1/register")
+                                                        .permitAll();
+                                        authRequestConfig.anyRequest().authenticated();
+                                });
+                return http.build();
+        }
 
-                    // Rutas públicas
-                    authRequestConfig.requestMatchers(HttpMethod.GET, "/api/v1/auth/is-token-valid").permitAll();
-                    authRequestConfig.requestMatchers(HttpMethod.GET, "/api/v1/auth/validate").permitAll();
-                    authRequestConfig.requestMatchers(HttpMethod.GET, "/api/v1/servicios").permitAll();
-                    authRequestConfig.requestMatchers(HttpMethod.GET, "/portal/listaSerivicios").permitAll();
-                    authRequestConfig.requestMatchers(HttpMethod.GET, "/portal/servicioPodo/{id}").permitAll();
-                    // Faltaba agrega esta url para que no tie problemas de cors
-                    // Esto deberia ser un PUT?
-                    authRequestConfig.requestMatchers(HttpMethod.GET, "/Turnos/cancelarTurno/{id}")
-                            .hasRole(Role.USER.name());
-                    authRequestConfig.requestMatchers(HttpMethod.GET, "/Turnos/listaTurnos/{id}")
-                            .hasRole(Role.USER.name());
-                    //  authRequestConfig.requestMatchers(HttpMethod.GET, "/Turnos/turnoDelDia/{date}").permitAll();
-                    // Faltaba agrega esta url para que no tie problemas de cors
-                    authRequestConfig.requestMatchers(HttpMethod.POST, "/api/v1/auth/authenticate").permitAll();
-                    authRequestConfig.requestMatchers(HttpMethod.POST, "/api/v1/auth/google").permitAll();
-                    authRequestConfig.requestMatchers(HttpMethod.POST, "/api/v1/auth/send-email").permitAll();
-                    authRequestConfig.requestMatchers(HttpMethod.POST, "/api/v1/register").permitAll();
-                    // Faltaba agrega esta url para que no tie problemas de --> Chekear si solo se
-                    // le debe permitir al usuario!
-//                    authRequestConfig.requestMatchers(HttpMethod.POST,
-//                            "/Turnos/reservarTurno/{turnoId}/{servicioId}/{usuarioid}").permitAll();
-                    // authRequestConfig.requestMatchers("/oauth2/**").permitAll();
-                    // authRequestConfig.requestMatchers("/login/oauth2/**").permitAll(); ;
-                    authRequestConfig.anyRequest().authenticated();// Permitir acceso a todos los endpoints de OAuth2
-                });
-
-        return http.build();
-    }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://podogonnet.netlify.app",
-                "https://podofrontdeploy.onrender.com", "http://localhost:5173", "https://accounts.google.com"));
-        configuration.setAllowedMethods(Arrays.asList("*"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
-        configuration.setAllowCredentials(true);
-        configuration.setExposedHeaders(Arrays.asList("Set-Cookie"));
-        // configuration.setExposedHeaders(Arrays.asList("Set-Cookie"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+        @Bean
+        CorsConfigurationSource corsConfigurationSource() {
+                CorsConfiguration configuration = new CorsConfiguration();
+                configuration.setAllowedOrigins(Arrays.asList("https://podogonnet.netlify.app",
+                                "https://podofrontdeploy.onrender.com", "http://localhost:5173",
+                                "https://accounts.google.com"));
+                configuration.setAllowedMethods(Arrays.asList("*"));
+                configuration.setAllowedHeaders(Arrays.asList("*"));
+                configuration.setAllowCredentials(true);
+                configuration.setExposedHeaders(Arrays.asList("Set-Cookie"));
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", configuration);
+                return source;
+        }
 }
